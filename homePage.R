dbc81588ab97283b32b63503b70b1168111353f8
Nyondo Musengah <musengah.nyondo@cs.unza.zm>
2023-05-30 16:58:21 +0200

    Added the ui for the homepage

--- homePage.R
+++ homePage.R
@@ -1,2 +1,12 @@
 # UI for the homepage
-homePageUi <- tags$h2("homepage") # define the home page interface here
+homePageUi <-sidebarLayout(
+    sidebarPanel(
+      # Input fields for filters
+      selectInput("genre", "Genre", choices = c("All", getGenres())),
+      actionButton("filter_btn", "Apply Filters")
+    ),
+    mainPanel(
+      # Output table for displaying movies
+      tableOutput("movies_table")
+    )
+  )
